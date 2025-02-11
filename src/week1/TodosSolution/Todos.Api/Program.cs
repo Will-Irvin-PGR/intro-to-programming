553fc5a0745c6441984d4d1dc5aefbccafe4d005
Will Irvin <will_m_irvin@progressive.com>
2025-02-11 15:17:47 -0600

    Added API call to angular frontend

--- src/week1/TodosSolution/Todos.Api/Program.cs
+++ src/week1/TodosSolution/Todos.Api/Program.cs
@@ -5,6 +5,17 @@ using Todos.Api.Todos;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(pol =>
+{
+    // this is demo code - refer to your local authorities here.
+    pol.AddDefaultPolicy(c =>
+    {
+        c.AllowAnyHeader();
+        c.AllowAnyMethod();
+        c.AllowAnyOrigin();
+    });
+});
+
 // Add services to the container.
 builder.Services.AddAuthorization();
 
@@ -20,6 +31,9 @@ builder.Services.AddMarten(builder =>
 
 // Above line is config for inside app
 var app = builder.Build();
+
+// Activate cors defined above
+app.UseCors();
 // Below is config for HTTP requests/responses
 
 // Configure the HTTP request pipeline.
--- src/week1/frontend/src/app/pages/todo-list.component.ts
+++ src/week1/frontend/src/app/pages/todo-list.component.ts
@@ -1,30 +1,38 @@
-import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  resource,
+} from '@angular/core';
 import { TodoListItem } from '../models';
 
 @Component({
   selector: 'app-todo-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ` @for (item of items(); track item.id) {
-    <div class="card bg-base-100 w-96 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">{{ item.description }}</h2>
-        <p>You Added this on {{ item.createdOn }}</p>
-        @if (item.completed == false) {
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Mark Completed</button>
-          </div>
-        } @else {
-          <p>You completed this item on {{ item.completedOn }}</p>
-          <button class="btn btn-primary">Remove from List</button>
-        }
+  template: ` @if (itemsResource.error()) {
+      <p>Could Not Load Your Data For Reasons. Sorry.</p>
+    }
+    @for (item of itemsResource.value(); track item.id) {
+      <div class="card bg-base-100 w-96 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">{{ item.description }}</h2>
+          <p>You Added this on {{ item.createdOn }}</p>
+          @if (item.completed === false) {
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Mark Completed</button>
+            </div>
+          } @else {
+            <p>You completed this item on {{ item.completedOn }}</p>
+            <button class="btn btn-primary">Remove from List</button>
+          }
+        </div>
       </div>
-    </div>
-  }`,
+    }`,
   styles: ``,
 })
 export class TodoListComponent {
-  items = signal<TodoListItem[]>([
+  /*   items = signal<TodoListItem[]>([
     {
       id: '99',
       completed: false,
@@ -38,5 +46,9 @@ export class TodoListComponent {
       description: 'Make Tacos',
       completedOn: '2025-02-11T20:58:47.300Z',
     },
-  ]);
+  ]); */
+
+  itemsResource = resource({
+    loader: () => fetch('http://localhost:1337/todos').then((r) => r.json()),
+  });
 }
