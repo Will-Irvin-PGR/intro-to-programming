bcf30be2811a26d79c95c7878bc588b1a89a4741
Will Irvin <will_m_irvin@progressive.com>
2025-02-12 14:25:49 -0600

    Finished testing demo for the day

--- src/week1/BankingSolution/Banking Tests/Accounts/MakingWithdrawals.cs
+++ src/week1/BankingSolution/Banking Tests/Accounts/MakingWithdrawals.cs
@@ -3,15 +3,30 @@
 namespace Banking_Tests.Accounts;
 public class MakingWithdrawals
 {
-    [Fact]
-    public void MakingWithdrawalsDecreasesTheBalance()
+    [Theory]
+    [InlineData(42.23)]
+    [InlineData(3.23)]
+    [InlineData(5000)] // Can take full balance
+    [InlineData(5000.01)] // Overdraft
+    public void MakingWithdrawalsDecreasesTheBalance(decimal amountToWithdraw)
     {
         var account = new Account();
         var openingBalance = account.GetBalance();
-        var amountToWithdraw = 42.23M;
 
         account.Withdraw(amountToWithdraw);
 
         Assert.Equal(openingBalance - amountToWithdraw, account.GetBalance());
+    }
+
+    [Fact (Skip = "Finish in morning")]
+    public void OverdraftNotAllowed()
+    {
+        var account = new Account();
+        var openingBalance = account.GetBalance();
+        var amountToWithdraw = openingBalance + .01M;
+
+        account.Withdraw(amountToWithdraw);
+
+        Assert.Equal(openingBalance, account.GetBalance());
     }
 }
