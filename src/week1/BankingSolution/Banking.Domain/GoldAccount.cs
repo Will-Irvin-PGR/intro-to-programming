1eb09391283f3f82ac1247fdc067e7dafc162fcd
Will Irvin <will_m_irvin@progressive.com>
2025-02-13 11:35:58 -0600

    Gold accounts w/ inheritance

---
+++ src/week1/BankingSolution/Banking.Domain/GoldAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace Banking.Domain;
+public class GoldAccount : Account
+{
+    public override void Deposit(decimal amountToDeposit)
+    {
+        base.Deposit(amountToDeposit * 1.2M);
+    }
+}
