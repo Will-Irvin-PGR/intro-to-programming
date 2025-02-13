c99d22ee6ea5f564fbe2233c90a55c585887463c
Will Irvin <will_m_irvin@progressive.com>
2025-02-13 11:11:34 -0600

    Before fixing gold accont

---
+++ src/week1/BankingSolution/Banking Tests/Accounts/GoldAccounts/GetBonusOnDeposits.cs
@@ -0,0 +1,19 @@
+﻿using Banking.Domain;
+
+namespace Banking_Tests.Accounts.GoldAccounts;
+public class GetBonusOnDeposits
+{
+    [Fact]
+    public void GetBonus()
+    {
+        var account = new Account();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+        var expectedBonus = 20M;
+        var expectedNewBalance = openingBalance + amountToDeposit + expectedBonus;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(expectedNewBalance, account.GetBalance());
+    }
+}
