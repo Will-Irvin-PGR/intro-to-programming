5617a6a53f24e85e2855a328c1157760c0e7b60e
Will Irvin <will_m_irvin@progressive.com>
2025-02-18 15:34:13 -0600

    Continued string kata still not working

--- src/week1/string-calculator-fri/StringCalculator/Calculator.cs
+++ src/week1/string-calculator-fri/StringCalculator/Calculator.cs
@@ -12,7 +12,7 @@ public class Calculator
 
         string pattern = "[,\n]";
 
-        string delimPattern = "//(.)\\|//\\[(.*)\\]\\";
+        string delimPattern = @"//(.)\|//\[(.*)\]\\";
 
         //if (numbers.Length > 4 &&
         //    numbers[0] == '/' && numbers[1] == '/' && numbers[3] == '\\')
@@ -21,8 +21,24 @@ public class Calculator
         //    numbers = numbers.Substring(4);
         //}
 
-        Regex.Matches(numbers, delimPattern);
+        var matches = Regex.Matches(numbers, delimPattern);
+        int length = 0;
 
+        foreach (Match match in matches)
+        {
+            pattern = pattern.Substring(0, pattern.Length - 1) + match.Value + ']';
+            length += match.Length;
+        }
+        if (length > 1)
+        {
+            length += 5;
+        } 
+        else if (length == 1)
+        {
+            length += 3;
+        }
+
+        numbers = numbers.Substring(length);
         try
         {
             var numList = Regex.Split(numbers, pattern).Select(int.Parse);
