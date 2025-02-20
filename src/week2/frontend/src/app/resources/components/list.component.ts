a652d744de40fda2315aa113b0c91feb710d295d
Will Irvin <will_m_irvin@progressive.com>
2025-02-20 14:42:08 -0600

    Added filtering capability to web app

--- src/week2/frontend/src/app/resources/components/list.component.ts
+++ src/week2/frontend/src/app/resources/components/list.component.ts
@@ -7,17 +7,18 @@ import {
 } from '@angular/core';
 import { LinkDocsDisplayItemComponent } from './link-docs-display-item.component';
 import { ResourceStore } from '../services/resource.store';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { map } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-resources-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [LinkDocsDisplayItemComponent],
+  imports: [LinkDocsDisplayItemComponent, RouterLink],
   template: `
     @if (store.filteredBy() !== null) {
       <p>Filtered by: {{ store.filteredBy() }}</p>
+      <a [routerLink]="['.']" class="btn btn-xs btn-secondary">Clear Filter</a>
     }
     <div
       class="grid grid-cols-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-4"
