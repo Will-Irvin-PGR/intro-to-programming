f0877891a7582af5542f20dcb47623ba126114a6
Will Irvin <will_m_irvin@progressive.com>
2025-02-20 14:37:15 -0600

    Added filtering capability to web app

--- src/week2/frontend/src/app/resources/components/list.component.ts
+++ src/week2/frontend/src/app/resources/components/list.component.ts
@@ -3,19 +3,26 @@ import {
   Component,
   inject,
   resource,
+  signal,
 } from '@angular/core';
 import { LinkDocsDisplayItemComponent } from './link-docs-display-item.component';
 import { ResourceStore } from '../services/resource.store';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-resources-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [LinkDocsDisplayItemComponent],
   template: `
+    @if (store.filteredBy() !== null) {
+      <p>Filtered by: {{ store.filteredBy() }}</p>
+    }
     <div
       class="grid grid-cols-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-4"
     >
-      @for (link of store.entities(); track link.id) {
+      @for (link of store.filteredResourceList(); track link.id) {
         <app-link-docs-display-item [link]="link" />
       } @empty {
         <p>You don't have any resources! Add Some?</p>
@@ -26,6 +33,18 @@ import { ResourceStore } from '../services/resource.store';
 })
 export class ListComponent {
   store = inject(ResourceStore);
+
+  activatedRoute = inject(ActivatedRoute);
+
+  constructor() {
+    this.activatedRoute.queryParams
+      .pipe(
+        takeUntilDestroyed(),
+        map((params) => params['filter']),
+        map((f) => (f === undefined ? null : f)),
+      )
+      .subscribe((v) => this.store.setFilteredBy(v));
+  }
 }
 // links = signal<ResourceListItem[]>([
 //   {
--- src/week2/frontend/src/app/resources/services/resource.store.ts
+++ src/week2/frontend/src/app/resources/services/resource.store.ts
@@ -1,18 +1,41 @@
-import { patchState, signalStore, withHooks, withMethods } from '@ngrx/signals';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withHooks,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
 import { addEntities, addEntity, withEntities } from '@ngrx/signals/entities';
 import { ResourceListItem, ResourceListItemCreateModel } from '../types';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { mergeMap, pipe, switchMap, tap } from 'rxjs';
 import { ResourceDataService } from './resource-data.service';
-import { inject } from '@angular/core';
+import { computed, inject } from '@angular/core';
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
 
 export const ResourceStore = signalStore(
   withDevtools('resources'),
+  withState<{ filteredBy: string | null }>({
+    filteredBy: null,
+  }),
   withEntities<ResourceListItem>(),
+  withComputed((store) => {
+    return {
+      filteredResourceList: computed(() => {
+        const filteredBy = store.filteredBy();
+        if (filteredBy === null) {
+          return store.entities();
+        }
+        return store.entities().filter((r) => r.tags.includes(filteredBy));
+      }),
+    };
+  }),
   withMethods((store) => {
     const service = inject(ResourceDataService);
     return {
+      setFilteredBy: (filteredBy: string | null) =>
+        patchState(store, { filteredBy: filteredBy }),
       add: rxMethod<ResourceListItemCreateModel>(
         pipe(
           mergeMap((item) =>
