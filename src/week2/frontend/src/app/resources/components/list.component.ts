bb4206185f60caf6d63243245fd7c6a71503be0e
Will Irvin <will_m_irvin@progressive.com>
2025-02-20 13:35:39 -0600

    Used grids for web layout

--- src/week2/frontend/src/app/resources/components/list.component.ts
+++ src/week2/frontend/src/app/resources/components/list.component.ts
@@ -12,7 +12,9 @@ import { ResourceStore } from '../services/resource.store';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [LinkDocsDisplayItemComponent],
   template: `
-    <div class="flex gap-4">
+    <div
+      class="grid grid-cols-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-4"
+    >
       @for (link of store.entities(); track link.id) {
         <app-link-docs-display-item [link]="link" />
       } @empty {
